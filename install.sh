130dc265bb34201fe20b4a4b9ed5e15cf64f22d2
Oliver Wiegers <oliver.wiegers@gmail.com>
2017-06-25 15:35:01 +0200

    install script fix

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!$PREFIX/bin/bash
 
+#backup
+if [ -d "$HOME/.termux" ]; then
+	mv $HOME/.termux $HOME/.termux.bak
+fi
 # install software
 apt update -y && apt upgrade -y
-apt install nodejs tmux unzip tar fontconfig wget git vim-python zsh curl ranger -y
-
+apt install python-dev openssh man make autoconf libllvm cmake nodejs tmux unzip tar fontconfig wget git vim-python zsh curl ranger clang -y 
 #clone repo
 git clone --recursive https://github.com/chrootzius/awesomeTermux.git .
 
@@ -11,21 +14,22 @@ git clone --recursive https://github.com/chrootzius/awesomeTermux.git .
 git clone --recursive https://github.com/chrootzius/vim_config.git .vim
 ln -sf /data/data/com.termux/files/home/.vim/.vimrc /data/data/com.termux/files/home/.vimrc
 
-#backup
-if [ -d "$HOME/.termux" ]; then
-	mv $HOME/.termux $HOME/.termux.bak
-fi
-
 #zsh
 git clone https://github.com/zsh-users/zsh-completions.git .oh-my-zsh/custom/pluins/zsh-completions
-cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
 chsh -s zsh
 
 #settings
 termux-setup-storage
-curl -fsLo $HOME/.termux/colors.properties --create-dirs https://cdn.rawgit.com/chrootzius/awsomeTermux/master/.termux/colors.properties
 
-#vim powerline
+#vim 
 pip install powerline-status
-print 'Add "vi-mode jump git zsh-competions sudo" to the plugin section of your .zshrc'
+cd ".vim/pack/plugins/start/YouCompleteMe/"
+termux-fix-shebang install.py
+./install.py
+cd
+
+#recreate directory structure
+mkdir -p documents/scripts/
+mkdir projects/
+
 exit
